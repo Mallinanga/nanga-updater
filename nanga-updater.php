ccef294482b9dad1e187326ac6317fbfb4829ea4
Panos Paganis <mallinanga@gmail.com>
2017-06-23 10:31:56 +0300

    WIP

--- nanga-updater.php
+++ nanga-updater.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Author URI: http://www.vgwebthings.com/
- * Author: VG web things
+ * Author URI: https://github.com/Mallinanga/
+ * Author: Panos Paganis
  * Description: VG web things Updater.
  * Plugin Name: VG web things Updater
- * Version: 1.2.0
+ * Version: 1.2.1
  */
 if ( ! defined('ABSPATH')) {
     exit;
 }
-define('NANGA_UPDATER_VERSION', '1.2.0');
+define('NANGA_UPDATER_VERSION', '1.2.1');
 
 require_once(dirname(__FILE__) . '/vendor/autoload.php');
 
