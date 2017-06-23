c5790985340a53d03de47272c786bcd19253e738
Panos Paganis <mallinanga@gmail.com>
2017-06-23 09:30:04 +0300

    Revert "WIP"
    This reverts commit 941523822736e3a0dc8a21d27ab7276ef1ee73f5.

--- src/Plugin.php
+++ src/Plugin.php
@@ -21,14 +21,13 @@ class Plugin
 
     public function check($transient)
     {
-        error_log('--- ' . __FUNCTION__ . ' ' . $this->config['slug']);
-        if ( ! file_exists(WP_PLUGIN_DIR . '/' . $this->config['plugin_file'])) {
-            return $transient;
-        }
         if (empty($transient->checked)) {
             return $transient;
         }
-        // delete_site_transient($this->config['slug'] . '_latest_tag');
+        delete_site_transient($this->config['slug'] . '_latest_tag');
+        if ( ! file_exists(WP_PLUGIN_DIR . '/' . $this->config['plugin_file'])) {
+            return $transient;
+        }
         $this->inject();
         $update = version_compare($this->config['new_version'], $this->config['version'], '>');
         if ($update) {
@@ -67,9 +66,11 @@ class Plugin
     private function latest()
     {
         $latest = get_site_transient($this->config['slug'] . '_latest_tag');
-        if ($this->force() || empty($latest)) {
+        if ($this->ignoreTransients() || empty($latest)) {
             $remoteResponse = wp_remote_get(trailingslashit($this->config['api_url']) . $this->config['channel'] . $this->github_auth);
             if (is_wp_error($remoteResponse)) {
+                error_log(print_r($remoteResponse, true));
+
                 return false;
             }
             $releases = json_decode($remoteResponse['body']);
@@ -93,37 +94,33 @@ class Plugin
         return $latest;
     }
 
-    private function force()
+    private function ignoreTransients()
     {
         return (defined('NANGA_UPDATER_FORCE_UPDATE') && NANGA_UPDATER_FORCE_UPDATE);
     }
 
     public function pluginDetails($false, $action, $details)
     {
-        error_log('--- ' . __FUNCTION__);
-        if (isset($details->slug) && $details->slug === $this->config['slug']) {
-            error_log(print_r($this->config['slug'], true));
-            $this->inject();
-            //$details->requires    = get_bloginfo('version');
-            $details->slug          = $this->config['slug'];
-            $details->plugin        = $this->config['slug'];
-            $details->name          = $this->config['plugin_name'];
-            $details->plugin_name   = $this->config['plugin_name'];
-            $details->version       = $this->config['new_version'];
-            $details->author        = $this->config['author'];
-            $details->homepage      = $this->config['homepage'];
-            $details->tested        = get_bloginfo('version');
-            $details->last_updated  = $this->config['last_updated'];
-            $details->sections      = [
-                'description' => $this->config['description'],
-                'changelog'   => $this->config['description'],
-            ];
-            $details->download_link = $this->config['zip_url'];
-
-            return $details;
+        if ( ! isset($details->slug) || $details->slug != $this->config['slug']) {
+            return false;
         }
+        $this->inject();
+        //$details->requires    = get_bloginfo('version');
+        $details->slug          = $this->config['slug'];
+        $details->plugin        = $this->config['slug'];
+        $details->name          = $this->config['plugin_name'];
+        $details->plugin_name   = $this->config['plugin_name'];
+        $details->version       = $this->config['new_version'];
+        $details->author        = $this->config['author'];
+        $details->homepage      = $this->config['homepage'];
+        $details->tested        = get_bloginfo('version');
+        $details->last_updated  = $this->config['last_updated'];
+        $details->sections      = [
+            'description' => $this->config['description'],
+        ];
+        $details->download_link = $this->config['zip_url'];
 
-        return $false;
+        return $details;
     }
 
     public function location($source, $remote_source, $upgrader, $hook_extra = null)
@@ -131,13 +128,19 @@ class Plugin
         global $wp_filesystem;
         //error_log(print_r($upgrader, true));
         if ($upgrader instanceof \Plugin_Upgrader) {
-            //error_log(print_r($_REQUEST, true));
+            //error_log(print_r($hook_extra, true));
+            //error_log(print_r($_POST, true));
             if ($_POST['slug'] == $this->config['slug']) {
+                //$new_source = WP_PLUGIN_DIR . '/' . trailingslashit($this->config['proper_folder_name']);
                 $new_source = trailingslashit($remote_source) . $this->config['proper_folder_name'];
+                //error_log(print_r($source, true));
+                //error_log(print_r($new_source, true));
                 //$wp_filesystem->move($upgrader['destination'], $new_source); // Forces 500 Error
                 if ($wp_filesystem->move($source, $new_source, true)) {
                     return trailingslashit($new_source);
                 } else {
+                    //error_log(print_r($upgrader, true));
+
                     return new \WP_Error();
                 }
             }
@@ -159,7 +162,7 @@ class Plugin
             $remoteData = $this->github_data;
         } else {
             $remoteData = get_site_transient($this->config['slug'] . '_github_data');
-            if ($this->force() || ( ! isset($remoteData) || ! $remoteData || '' == $remoteData)) {
+            if ($this->ignoreTransients() || ( ! isset($remoteData) || ! $remoteData || '' == $remoteData)) {
                 $remoteData = wp_remote_get($this->config['api_url'] . $this->github_auth);
                 if (is_wp_error($remoteData)) {
                     error_log(print_r($remoteData, true));
