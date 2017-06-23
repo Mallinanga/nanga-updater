955660120d1bb3967b03d00a50983d04eb7c569b
Panos Paganis <mallinanga@gmail.com>
2017-06-23 10:30:27 +0300

    Revert "Revert "WIP""
    This reverts commit c5790985340a53d03de47272c786bcd19253e738.

--- src/Plugin.php
+++ src/Plugin.php
@@ -21,13 +21,14 @@ class Plugin
 
     public function check($transient)
     {
-        if (empty($transient->checked)) {
-            return $transient;
-        }
-        delete_site_transient($this->config['slug'] . '_latest_tag');
+        error_log('--- ' . __FUNCTION__ . ' ' . $this->config['slug']);
         if ( ! file_exists(WP_PLUGIN_DIR . '/' . $this->config['plugin_file'])) {
             return $transient;
         }
+        if (empty($transient->checked)) {
+            return $transient;
+        }
+        // delete_site_transient($this->config['slug'] . '_latest_tag');
         $this->inject();
         $update = version_compare($this->config['new_version'], $this->config['version'], '>');
         if ($update) {
@@ -66,11 +67,9 @@ class Plugin
     private function latest()
     {
         $latest = get_site_transient($this->config['slug'] . '_latest_tag');
-        if ($this->ignoreTransients() || empty($latest)) {
+        if ($this->force() || empty($latest)) {
             $remoteResponse = wp_remote_get(trailingslashit($this->config['api_url']) . $this->config['channel'] . $this->github_auth);
             if (is_wp_error($remoteResponse)) {
-                error_log(print_r($remoteResponse, true));
-
                 return false;
             }
             $releases = json_decode($remoteResponse['body']);
@@ -94,33 +93,37 @@ class Plugin
         return $latest;
     }
 
-    private function ignoreTransients()
+    private function force()
     {
         return (defined('NANGA_UPDATER_FORCE_UPDATE') && NANGA_UPDATER_FORCE_UPDATE);
     }
 
     public function pluginDetails($false, $action, $details)
     {
-        if ( ! isset($details->slug) || $details->slug != $this->config['slug']) {
-            return false;
-        }
-        $this->inject();
-        //$details->requires    = get_bloginfo('version');
-        $details->slug          = $this->config['slug'];
-        $details->plugin        = $this->config['slug'];
-        $details->name          = $this->config['plugin_name'];
-        $details->plugin_name   = $this->config['plugin_name'];
-        $details->version       = $this->config['new_version'];
-        $details->author        = $this->config['author'];
-        $details->homepage      = $this->config['homepage'];
-        $details->tested        = get_bloginfo('version');
-        $details->last_updated  = $this->config['last_updated'];
-        $details->sections      = [
-            'description' => $this->config['description'],
-        ];
-        $details->download_link = $this->config['zip_url'];
+        error_log('--- ' . __FUNCTION__);
+        if (isset($details->slug) && $details->slug === $this->config['slug']) {
+            error_log(print_r($this->config['slug'], true));
+            $this->inject();
+            //$details->requires    = get_bloginfo('version');
+            $details->slug          = $this->config['slug'];
+            $details->plugin        = $this->config['slug'];
+            $details->name          = $this->config['plugin_name'];
+            $details->plugin_name   = $this->config['plugin_name'];
+            $details->version       = $this->config['new_version'];
+            $details->author        = $this->config['author'];
+            $details->homepage      = $this->config['homepage'];
+            $details->tested        = get_bloginfo('version');
+            $details->last_updated  = $this->config['last_updated'];
+            $details->sections      = [
+                'description' => $this->config['description'],
+                'changelog'   => $this->config['description'],
+            ];
+            $details->download_link = $this->config['zip_url'];
 
-        return $details;
+            return $details;
+        }
+
+        return $false;
     }
 
     public function location($source, $remote_source, $upgrader, $hook_extra = null)
@@ -128,19 +131,13 @@ class Plugin
         global $wp_filesystem;
         //error_log(print_r($upgrader, true));
         if ($upgrader instanceof \Plugin_Upgrader) {
-            //error_log(print_r($hook_extra, true));
-            //error_log(print_r($_POST, true));
+            //error_log(print_r($_REQUEST, true));
             if ($_POST['slug'] == $this->config['slug']) {
-                //$new_source = WP_PLUGIN_DIR . '/' . trailingslashit($this->config['proper_folder_name']);
                 $new_source = trailingslashit($remote_source) . $this->config['proper_folder_name'];
-                //error_log(print_r($source, true));
-                //error_log(print_r($new_source, true));
                 //$wp_filesystem->move($upgrader['destination'], $new_source); // Forces 500 Error
                 if ($wp_filesystem->move($source, $new_source, true)) {
                     return trailingslashit($new_source);
                 } else {
-                    //error_log(print_r($upgrader, true));
-
                     return new \WP_Error();
                 }
             }
@@ -162,7 +159,7 @@ class Plugin
             $remoteData = $this->github_data;
         } else {
             $remoteData = get_site_transient($this->config['slug'] . '_github_data');
-            if ($this->ignoreTransients() || ( ! isset($remoteData) || ! $remoteData || '' == $remoteData)) {
+            if ($this->force() || ( ! isset($remoteData) || ! $remoteData || '' == $remoteData)) {
                 $remoteData = wp_remote_get($this->config['api_url'] . $this->github_auth);
                 if (is_wp_error($remoteData)) {
                     error_log(print_r($remoteData, true));
---
+++ src/Plugins/Newsletter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Nanga\Plugins;
+
+use Nanga\Plugin;
+
+class Newsletter extends Plugin
+{
+
+    public $config = [];
+
+    public function __construct()
+    {
+        $this->config = [
+            'api_url'            => 'https://api.github.com/repos/Mallinanga/nanga-newsletter',
+            'channel'            => 'tags',
+            'github_url'         => 'https://github.com/Mallinanga/nanga-newsletter',
+            'plugin_file'        => 'nanga-newsletter/nanga-newsletter.php',
+            'proper_folder_name' => 'nanga-newsletter',
+            'slug'               => 'nanga-newsletter',
+            'zip_url'            => 'https://api.github.com/repos/Mallinanga/nanga-newsletter/zipball',
+        ];
+        parent::__construct();
+    }
+}
