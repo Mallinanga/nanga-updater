b9726f9faa81cdf0f773a6ef1aeebecabcdd6fbe
Panos Paganis <mallinanga@gmail.com>
2017-07-07 22:52:31 +0300

    WIP

--- nanga-updater.php
+++ nanga-updater.php
@@ -4,18 +4,18 @@
  * Author: Panos Paganis
  * Description: VG web things Updater.
  * Plugin Name: VG web things Updater
- * Version: 1.4.0
+ * Version: 1.4.1
  */
 
 defined('WPINC') || die;
-
-define('NANGA_UPDATER_VERSION', '1.4.0');
-define('NANGA_UPDATER_DIR_URL', plugin_dir_url(__FILE__));
-
+define('NANGA_UPDATER_VERSION', '1.4.1');
+if (defined('NANGA_VIP_URL')) {
+    define('NANGA_UPDATER_DIR_URL', NANGA_VIP_URL . 'plugins/nanga-updater/');
+} else {
+    define('NANGA_UPDATER_DIR_URL', plugin_dir_url(__FILE__));
+}
 require_once(dirname(__FILE__) . '/vendor/autoload.php');
-
 register_activation_hook(__FILE__, ['\Nanga\PluginUpdater', 'activate']);
 register_deactivation_hook(__FILE__, ['\Nanga\PluginUpdater', 'deactivate']);
-
 \Nanga\PluginUpdater::init();
 // \Nanga\PluginUpdater::auto();
